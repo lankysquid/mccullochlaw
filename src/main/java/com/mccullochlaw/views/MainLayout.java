4d47d0eaa9b1dd074f8da625d5a03191cd7723a9
start.vaadin.com <no-reply@vaadin.com>
2022-08-23 21:32:28 +0000

    Generated project

---
+++ src/main/java/com/mccullochlaw/views/MainLayout.java
@@ -0,0 +1,84 @@
+package com.mccullochlaw.views;
+
+
+import com.mccullochlaw.components.appnav.AppNav;
+import com.mccullochlaw.components.appnav.AppNavItem;
+import com.mccullochlaw.views.about.AboutView;
+import com.mccullochlaw.views.home.HomeView;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Footer;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.router.PageTitle;
+
+/**
+ * The main view is a top-level placeholder for other views.
+ */
+public class MainLayout extends AppLayout {
+
+    private H1 viewTitle;
+
+    public MainLayout() {
+        setPrimarySection(Section.DRAWER);
+        addToNavbar(true, createHeaderContent());
+        addToDrawer(createDrawerContent());
+    }
+
+    private Component createHeaderContent() {
+        DrawerToggle toggle = new DrawerToggle();
+        toggle.addClassNames("view-toggle");
+        toggle.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+        toggle.getElement().setAttribute("aria-label", "Menu toggle");
+
+        viewTitle = new H1();
+        viewTitle.addClassNames("view-title");
+
+        Header header = new Header(toggle, viewTitle);
+        header.addClassNames("view-header");
+        return header;
+    }
+
+    private Component createDrawerContent() {
+        H2 appName = new H2("mccullochlaw");
+        appName.addClassNames("app-name");
+
+        com.vaadin.flow.component.html.Section section = new com.vaadin.flow.component.html.Section(appName,
+                createNavigation(), createFooter());
+        section.addClassNames("drawer-section");
+        return section;
+    }
+
+    private AppNav createNavigation() {
+        // AppNav is not yet an official component.
+        // For documentation, visit https://github.com/vaadin/vcf-nav#readme
+        AppNav nav = new AppNav();
+        nav.addClassNames("app-nav");
+
+        nav.addItem(new AppNavItem("Home", HomeView.class, "la la-th-list"));
+        nav.addItem(new AppNavItem("About", AboutView.class, "la la-globe"));
+
+        return nav;
+    }
+
+    private Footer createFooter() {
+        Footer layout = new Footer();
+        layout.addClassNames("app-nav-footer");
+
+        return layout;
+    }
+
+    @Override
+    protected void afterNavigation() {
+        super.afterNavigation();
+        viewTitle.setText(getCurrentPageTitle());
+    }
+
+    private String getCurrentPageTitle() {
+        PageTitle title = getContent().getClass().getAnnotation(PageTitle.class);
+        return title == null ? "" : title.value();
+    }
+}
